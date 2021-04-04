c5c7e23da460795932f9693a5693caba645a0e5f
Bruno Furtado <brunofurmon@gmail.com>
2021-04-04 20:14:07 -0300

    Fix heavy test

--- test/domain/heavy.test.js
+++ test/domain/heavy.test.js
@@ -3,7 +3,7 @@ const { heavyCpuInMs } = require('../../src/domain/business/heavy');
 describe('domain -> heavy', () => {
     describe('heavyCpuInMs', () => {
         it('Should load CPU for ~5s', () => {
-            expect(heavyCpuInMs(5000)).toBeDefined();
+            expect(heavyCpuInMs(5000)).toBeUndefined();
         });
     });
 });
