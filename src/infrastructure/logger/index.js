0f03cbbe75f523ec3414265697ab354f0a4680c2
Bruno Furtado <brunofurmon@gmail.com>
2021-04-04 19:18:49 -0300

    Colored logger
    Signed-off-by: Bruno Furtado <brunofurmon@gmail.com>

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -1,3 +1,37 @@
-const logger = console;
+const { APP_PREFIX } = process.env;
+const internalLogger = console;
 
-module.exports = () => logger;
+module.exports = () => {
+    // Colors as seen on: https://gist.github.com/abritinthebay/d80eb99b2726c83feb0d97eab95206c4
+    const cyan = '\x1b[36m%s\x1b[0m';
+    const yellow = '\x1b[33m%s\x1b[0m';
+    const red = '\x1b[31m%s\x1b[0m';
+    const lightgray = '\x1b[37m%s\x1b[0m';
+
+    const prefix = () => {
+        return `  ${APP_PREFIX}:(${new Date().toISOString()}):`;
+    };
+
+    const info = (...args) => {
+        internalLogger.info(cyan, prefix(), ...args);
+    };
+
+    const warn = (...args) => {
+        internalLogger.warn(yellow, prefix(), ...args);
+    };
+
+    const error = (...args) => {
+        internalLogger.error(red, prefix(), ...args);
+    };
+
+    const debug = (...args) => {
+        internalLogger.debug(lightgray, prefix(), ...args);
+    };
+
+    return {
+        info,
+        warn,
+        error,
+        debug
+    };
+};
